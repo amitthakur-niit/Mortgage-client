5b18fb59caa7555b3e9a90160b99f8740e9455cc
Har-Bagga <59388066+Har-Bagga@users.noreply.github.com>
2020-01-13 14:38:18 +0530

    commit "How to Apply "
    commit "How to Apply "

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -10,6 +9,8 @@ import { FormListModule } from './form-list/form-list.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDesignModule } from './material-design/material-design.module';
 import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
--- src/app/form-list/how-to-apply/how-to-apply.component.ts
+++ src/app/form-list/how-to-apply/how-to-apply.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-how-to-apply',
   templateUrl: './how-to-apply.component.html',
-  styleUrls: ['./how-to-apply.component.scss']
+  styleUrls: ['./how-to-apply.component.scss'],
+ 
 })
 export class HowToApplyComponent implements OnInit {
+  
+  constructor(private formBuilder: FormBuilder,private router: Router) { 
+    // this.formGroup = this.formBuilder.group({
+    //   'Postcode': new FormControl(null, Validators.required),
+    //   'Address': new FormControl(null, Validators.required),
+    //   'propertyType': new FormControl(null, Validators.required),
+    //   'NumberOfBedrooms': new FormControl(null, Validators.required),
+    //   'propertyAge': new FormControl(null, Validators.required),
+    // });
 
-  constructor() { }
+  }
+
 
   ngOnInit() {
   }
